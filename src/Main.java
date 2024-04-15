02e790a2c55a59f00c5a46ff19658bca5e3c89d3
REIGaruki <gyakujo@mail.ru>
2024-04-15 13:18:36 +0300

    Print description

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Griffindor[] griffindorers = new Griffindor[]{
-                new Griffindor("Harry Potter", 55, 20, 90, 80, 80),
-                new Griffindor("Гермиона Грейнджер", 99, 76, 90, 60, 50),
-                new Griffindor("Рон Уизли", 36, 14, 30, 100, 90)
-        };
-        Slytherin[] slytheriners = new Slytherin[]{
+        Hogwarts[] students = new Hogwarts[]{
+               new Griffindor("Гарри Поттер", 55, 20, 90, 80, 80),
+               new Griffindor("Гермиона Грейнджер", 99, 76, 90, 60, 50),
+               new Griffindor("Рон Уизли", 36, 14, 30, 100, 90),
                new Slytherin("Драко Малфой", 77, 69, 60, 70, 100, 60, 90),
                new Slytherin("Грэхэм Монтегю", 80, 70, 10, 90, 70, 40, 70),
-               new Slytherin("Грегори Гойл", 49, 64, 50, 50, 60, 40, 80)
+               new Slytherin("Грегори Гойл", 49, 64, 50, 50, 60, 40, 80),
+               new Hufflepuff("Захария Смит", 67, 40, 57, 78, 91),
+               new Hufflepuff("Седрик Диггори", 94, 89, 65, 87, 69),
+               new Hufflepuff("Джастин Финч-Флетчли", 17, 20, 98, 84, 91),
+               new Ravenclaw("Чжоу Чанг", 65, 41, 67, 42, 55, 80),
+               new Ravenclaw("Падма Патил", 48, 62, 47, 85, 37, 74),
+               new Ravenclaw("Маркус Белби", 76, 89, 87, 69, 92, 25)
         };
-        Hufflepuff[] hufflepuffers = new Hufflepuff[]{
-                new Hufflepuff("Захария Смит", 67, 40, 57, 78, 91),
-                new Hufflepuff("Седрик Диггори", 94, 89, 65, 87, 69),
-                new Hufflepuff("Джастин Финч-Флетчли", 17, 20, 98, 84, 91)
-        };
-        Ravenclaw[] ravenclawers = new Ravenclaw[]{
-                new Ravenclaw("Чжоу Чанг", 65, 41, 67, 42, 55, 80),
-                new Ravenclaw("Падма Патил", 48, 62, 47, 85, 37, 74),
-                new Ravenclaw("Маркус Белби", 76, 89, 87, 69, 92, 25)
-        };
-        for (Ravenclaw ravenclawer: ravenclawers) {
-            System.out.println(ravenclawer);
-        }
-        for (Griffindor griffindorer: griffindorers) {
-            System.out.println(griffindorer);
-        }
-        for (Slytherin slytheriner: slytheriners) {
-            System.out.println(slytheriner);
-        }
-        for (Hufflepuff hufflepuffer: hufflepuffers) {
-            System.out.println(hufflepuffer);
+        for (Hogwarts student: students) {
+            System.out.println(student);
         }
     }
 }
